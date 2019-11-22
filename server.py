f43cd058b8d0a04f44479a55cf716af11294feb3
Vishwanatha Sai ananth <saiananth.kmit@gmail.com>
2019-11-22 17:39:53 -0500

    updated product Id

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import socket
 s = socket.socket()          
 print ("Socket successfully created")
 port = 12345
-productId = "3"
+productId = "4"
 s.bind(('', port))         
 print ("socket binded to %s" %(port))
 s.listen(5)      
